17264a9ab48738cd5eb31e8e939590a1e4593f0e
RimzimSoni-rs <sonirimzim3@gmail.com>
2024-08-05 18:00:03 +0530

    Add files via upload

---
+++ arithematicOperator.py
@@ -0,0 +1,7 @@
+print("____________BMI calculator_____________")
+a = input("Enter your weight(in kgs)  ")
+b = input ("Enter your height (in cms)  ")
+c = float(b)
+bmi = int(a) / (c * c)
+
+print("Your body mass index is : " + str(bmi))
---
+++ takingInput.py
@@ -0,0 +1,12 @@
+#byDefault the input function takes value as string.
+name = input("What is your name ? \n")
+
+print("Hello " + name + ", how are you? ")
+print(len(name))
+
+a = 5
+b ="soni"
+print(a)
+print(b)
+
+#we cannot concatinate a and b because one is number and other is string. e.g. print(a + b) will give an error!
