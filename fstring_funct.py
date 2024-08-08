2a6d824363716ce4e942edde1f1ea597004f98f7
RimzimSoni-rs <sonirimzim3@gmail.com>
2024-08-08 23:29:36 +0530

    Add files via upload

---
+++ fstring_funct.py
@@ -0,0 +1,17 @@
+#printing diff datatype variables in the same print funct. without typecasting
+name = "Rimzim"
+age = 19
+sgpa = 9.57
+
+print(f"My name is {name}. I am {age} years old. My last sem sgpa is {sgpa}")
+
+#Exercise using f-string funct
+age = int(input("Enter Your Age  "))
+calcAge = 90-int(age)
+a = calcAge*365
+b = calcAge*52
+c = calcAge*12
+
+
+print(f"You have {a} days, {b} weeks and {c} months left.")
+
