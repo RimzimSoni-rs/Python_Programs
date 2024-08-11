85515a5c0f92d6902c5543314b0286df4bd4eb86
RimzimSoni-rs <sonirimzim3@gmail.com>
2024-08-11 17:51:58 +0530

    Add files via upload

--- if_else.py
+++ if_else.py
@@ -1,6 +1,21 @@
 number = int(input("Enter a number: "))
 
+
+#find even-odd
 if number % 2 == 0:
     print("The number is even.")
 else:
     print("The number is odd.")
+
+#find prime-number
+count=0
+for i in range (1,10):
+    if number%i ==0:
+      count=count+1 
+if number ==0:
+    print("Zero is not a prime number")
+      
+if count >2: 
+      print ("It is not a prime number")
+else:
+   print("number is prime")
---
+++ leapYear.py
@@ -0,0 +1,5 @@
+year = int(input("Enter an year to check whether it is leap year or not"))
+if year%4==0 :
+    print(f"{year} is a leap year")
+else:
+ print(f"{year} is not a leap year")
