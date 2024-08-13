842a75bc11e9a25fc0deadab9fd1f5f8fb9c9836
RimzimSoni-rs <sonirimzim3@gmail.com>
2024-08-13 22:35:30 +0530

    Add files via upload

---
+++ sumNaturalNo.py
@@ -0,0 +1,10 @@
+print("Program to find sum of Natural numbers till number you enter. ")
+num = int(input("Enter a number : "))
+if num==0:
+    print("Enter a positive number ")
+else :
+    sum =0
+    while (num>=0):
+        sum += num
+        num -=1
+    print(sum)
