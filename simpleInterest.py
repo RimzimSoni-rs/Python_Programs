d39852ce4e5e3340c8fdfea27b5b1e79cba9f1b7
RimzimSoni-rs <sonirimzim3@gmail.com>
2024-08-24 22:18:55 +0530

    Add files via upload

---
+++ simpleInterest.py
@@ -0,0 +1,13 @@
+print('Enter principal, time period and rate of interest respectively : ')
+p = int(input())
+t = int(input())
+r = int(input())
+print('The principal is',p)
+print('The time period is',t)
+print('The rate of interest is',r)
+    
+
+si = (p * t * r)/100
+    
+print('The Simple Interest is', si)
+
