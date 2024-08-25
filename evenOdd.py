70daac6159eab91413d02d7aea18c737d77a1061
RimzimSoni-rs <sonirimzim3@gmail.com>
2024-08-25 23:34:39 +0530

    Add files via upload

---
+++ evenOdd.py
@@ -0,0 +1,8 @@
+num = int(input('Enter a Number (you can enter +ve, -ve or 0) : '))
+if num == 0:
+    print("number is zero ")
+elif num < 0:
+    print("number is negative ")
+else:
+    print("number is positive")
+          
