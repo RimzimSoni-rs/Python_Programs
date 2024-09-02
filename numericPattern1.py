67f1e9a504d5acbcebaac8e3e607c9b913c66940
Rimzim Soni <sonirimzim3@gmail.com>
2024-09-02 23:32:53 +0800

    Add files via upload

---
+++ numericPattern1.py
@@ -0,0 +1,17 @@
+for i in range(4):
+    for j in range(4):
+        print(j+1, end=" ")
+    print("")
+print()
+
+for i in range(4):
+    for j in range(i+1):
+        print(j+1," " , end="")
+    print()
+print()
+
+for i in range (4):
+    for j in range(4-i):
+        print(j+1," ", end="")
+    print()
+print()
